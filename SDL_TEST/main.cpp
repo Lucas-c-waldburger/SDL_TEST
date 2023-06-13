e0eedeb4e1c21627a25afd9ee472cd805b50cf5c
Lucas Waldburger <lucas.c.waldburger@gmail.com>
2023-06-13 10:28:30 -0400

    Initial Commit

---
+++ SDL_TEST/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  SDL_TEST
+//
+//  Created by Lower School Music on 6/13/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
